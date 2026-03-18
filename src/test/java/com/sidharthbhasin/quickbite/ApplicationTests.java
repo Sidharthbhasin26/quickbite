bcab672dda1bb37df6ac8c7af1548416d64fa8bf
sidharth <sidharthbhasin26@gmail.com>
2026-03-18 18:14:50 +0530

    Initial Project Upload

---
+++ src/test/java/com/sidharthbhasin/quickbite/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sidharthbhasin.quickbite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
